9e93f904d030287c4738c76e89861ac25d743285
bishimaru <a414510nee@yahoo.co.jp>
2024-08-01 18:46:57 +0900

    2

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-os.environ['TZ'] = 'UTC'
+
 
 def main():
     """Run administrative tasks."""
@@ -15,7 +15,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    import backports.zoneinfo as zoneinfo  # Add this line
     execute_from_command_line(sys.argv)
 
 
