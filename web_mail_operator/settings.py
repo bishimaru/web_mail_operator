46667f44dfdf807417b5643aa1f5874103b2e812
bishimaru <a414510nee@yahoo.co.jp>
2024-08-09 18:37:59 +0900

    777

--- web_mail_operator/settings.py
+++ web_mail_operator/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
 # サービス内でstaticフォルダのURLパスを設定
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
