ba1c4a9aaecf0e0418ed3bd5ca9c1474ec23944d
bishimaru <a414510nee@yahoo.co.jp>
2024-08-09 17:27:59 +0900

    777

--- web_mail_operator/settings.py
+++ web_mail_operator/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lljt#=aed=%1gnf2fy6fw&s1hbpv&j0(#%=k7pon3nd#k^j2f*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
