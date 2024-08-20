1a907e9d1daf6f25c921fbaf21fbc82286bd6c96
bishimaru <a414510nee@yahoo.co.jp>
2024-08-20 17:24:13 +0900

    auto commit

--- web_mail_operator/settings.py
+++ web_mail_operator/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # STATIC_ROOT は、本番環境で collectstatic コマンドを実行した際に、すべての静的ファイルが収集されるディレクトリです。
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # Default primary key field type
